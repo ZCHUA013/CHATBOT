91c441ab216dd93d2c3ee568afc9ba57bb9c3fee
Shawn Chua <zschua94@gmail.com>
2019-02-22 20:30:21 +0800

    add a running chatbot!!

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const Telegraf = require("telegraf");
+
+console.log("running....");
 const API_KEY = "696261432:AAGOdX8FLH06XnQAr1DvAgddKeoDsiK6UQQ";
 
+const bot = new Telegraf(API_KEY);
+
+//modern syntax allows me to write it shorter with deault return
+const greet2 = name => "Hello!!!!!!!!!!!!!!!" + name; 
+
+const helpHandler = ctx => ctx.reply("Hello!!!!!!!!!!!!!!!!!!");
+bot.help(helpHandler);
+
+bot.launch();
