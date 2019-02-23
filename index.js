a053ef962989a1b1b1d35716fa7fc9beef63e357
zschua94 <zschua94@gmail.com>
2019-02-23 13:02:21 +0800

    saturday

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const greet2 = name => "Hello!!!!!!!!!!!!!!!" + name;
 const helpHandler = ctx => ctx.reply("Hello!!!!!!!!!!!!!!!!!!");
 bot.help(helpHandler);
 
-bot.command("hello", context => context.reply("boo"));
+bot.command("hello", context => context.reply("HI KC"));
 bot.launch();
